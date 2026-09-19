ed59b4c979c8632bd344f7ea2b48d2d7c3bd18eb
Manthena Sree Haasini <haasinimanthena777@gmail.com>
2026-09-19 10:41:36 +0530

    Create app.js

---
+++ week 10/app4/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "./Header";
+import Student from "./Student";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="app">
+      <Header title="Learn Passing Function Parameter" />
+      <Student />
+    </div>
+  );
+}
+
+export default App;
