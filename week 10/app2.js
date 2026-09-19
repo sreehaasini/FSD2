057b1bdf413a3a8c638bd861044d546e6bb08154
Manthena Sree Haasini <haasinimanthena777@gmail.com>
2026-09-19 10:25:49 +0530

    Create app2.js

---
+++ week 10/app2.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+function App() {
+  const content = (
+    <div>
+      <h2 style={{ color: "blue" }}>Welcome to JSX</h2>
+
+      <p>JSX allows us to write HTML inside JavaScript.</p>
+
+      <ul>
+        <li>Easy to read</li>
+        <li>Looks like HTML</li>
+        <li>Used in React components</li>
+      </ul>
+    </div>
+  );
+
+  return (
+    <div>
+      <h1>Writing Markup with JSX</h1>
+
+      {/* Rendering JSX variable */}
+      {content}
+    </div>
+  );
+}
+
+export default App;
